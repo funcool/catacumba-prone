bbf13feca02644f147148807a4cb31f6b24152ef
Andrey Antukh <niwi@niwi.nz>
2016-02-06 09:21:09 +0200

    Set version to 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/catacumba-prone "0.2.0"
+(defproject funcool/catacumba-prone "0.2.1"
   :description "A prone integration for catacumba"
   :url "http://github.com/funcool/catacumba-prone"
   :license {:name "BSD (2-Clause)"
@@ -8,6 +8,6 @@
   :jar-exclusions [#"\.swp|\.swo|bench\.clj|user\.clj"]
   :javac-options ["-target" "1.8" "-source" "1.8"
                   "-Xlint:-options" "-Xlint:unchecked"]
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [funcool/catacumba "0.11.0" :scope "provided"]
-                 [prone "1.0.1"]])
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [funcool/catacumba "0.11.1" :scope "provided"]
+                 [prone "1.0.2"]])
