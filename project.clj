2c54bc2d8ee9bfa02117b5da5035b1db2697f5d2
Andrey Antukh <niwi@niwi.nz>
2016-05-26 22:19:48 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/funcool/catacumba-prone"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-
   :source-paths ["src"]
   :jar-exclusions [#"\.swp|\.swo|bench\.clj|user\.clj"]
   :javac-options ["-target" "1.8" "-source" "1.8"
                   "-Xlint:-options" "-Xlint:unchecked"]
+  :plugins [[lein-ancient "0.6.10"]]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [funcool/catacumba "0.11.1" :scope "provided"]
-                 [prone "1.0.2"]])
+                 [funcool/catacumba "0.16.0" :scope "provided"]
+                 [prone "1.1.1"]])
